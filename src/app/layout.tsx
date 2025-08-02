a52df3410f29bfd3227ad75359c81e20da505c71
Satyam Mishra <satyammishra9050@gmail.com>
2025-08-02 16:01:45 +0530

    feat: update deps & add vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
 import AuthWrapper from '@/components/AuthWrapper';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <AuthWrapper>
           {children}
+          <Analytics />
         </AuthWrapper>
         <Toaster position="top-right" />
       </body>
